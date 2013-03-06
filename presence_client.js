78373b20dd741961584f240e3d8e50183e937f8c
Tom Coleman <tom@thesnail.org>
2013-03-07 10:42:00 +1100

    Deal better with tracking when a user is off/on-line

--- presence_client.js
+++ presence_client.js
@@ -30,6 +30,9 @@ Meteor.autorun(function() {
   // read this, so the context is invalidated every time the interval changes
   Session.get('last-presence-set-at');
   
+  // also read userId so context is invalidated as soon as login state changes
+  Meteor.userId();
+  
   Meteor.call('setPresence', Meteor.Presence.sessionId, Meteor.Presence.state());
 });
 
--- presence_server.js
+++ presence_server.js
@@ -1,18 +1,25 @@
 PRESENCE_TIMEOUT_MS = 10000;
 PRESENCE_INTERVAL = 1000;
 
-
-
 // a method to indicate that the user is still online
 Meteor.methods({
   setPresence: function(sessionId, state) {
+    var update = {$set: {
+        state: state,
+        lastSeen: new Date()
+    }};
+    
+    // need to unset userId if it's not defined as they may have just logged out
+    var userId = Meteor.userId()
+    if (userId) {
+      update.$set.userId = userId;
+    } else {
+      update.$unset = {userId: true};
+    }
+    
     Meteor.presences.update({
       sessionId: sessionId
-    }, {$set: {
-      userId: Meteor.userId(),
-      state: state,
-      lastSeen: new Date()
-    }}, {
+    }, update, {
       upsert: true
     });
   }
