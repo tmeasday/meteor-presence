60aff56d9d7755ba944426088bd5160d7f2378c1
Tom Coleman <tom@thesnail.org>
2013-04-11 17:41:49 +1000

    Needs accounts.

--- package.js
+++ package.js
@@ -3,6 +3,8 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
+  api.use('accounts-base', ['client', 'server']);
+  
   api.add_files('presence_common.js', ['client', 'server']);
   api.add_files('presence_client.js', 'client');
   api.add_files('presence_server.js', 'server');
