e59a12739685972c970f70956c4d3e78c31818ec
David Burles <dburles@gmail.com>
2013-08-18 19:48:05 +1000

    Updated to work with Meteor 0.6.5

--- package.js
+++ package.js
@@ -4,6 +4,7 @@ Package.describe({
 
 Package.on_use(function (api) {
   api.use('accounts-base', ['client', 'server']);
+  api.use('session', 'client');
   
   api.add_files('presence_common.js', ['client', 'server']);
   api.add_files('presence_client.js', 'client');
